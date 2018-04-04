32157d190b503d1a4787e06de2d847aa93016fb0
am034402 <aaron.mcruer@cerner.com>
2018-04-04 13:10:39 -0500

    work.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='travis_talk',
+      version=0.1,
+      description='a testament to the arrogance and folly of man',
+      author='rusty shackleford',
+      license='none',
+      packages=['travis_talk'],
+      zip_safe=False)
